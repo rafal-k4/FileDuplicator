3fb23739e93de4d16135387967e8ab15e2325ed9
rafal-k4 <42941375+rafal-k4@users.noreply.github.com>
2019-01-23 22:48:21 +0100

    dependency injection in progress

--- FileDuplicator/DependencyInjection.cs
+++ FileDuplicator/DependencyInjection.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Text.RegularExpressions;
+using FileDuplicator.Configuration;
 
 namespace FileDuplicator
 {
     public class DependencyInjection
     {
         IServiceCollection service;
+        public IServiceProvider ServiceProvider { get; set; }
 
         public DependencyInjection()
         {
@@ -20,14 +24,39 @@ namespace FileDuplicator
         {
             //at this point all dependencies can be added
             //to the DI system via service collection
-
+            service.AddScoped(typeof(IAppLogic), typeof(AppLogic));
+            service.AddScoped(typeof(IPathRetriever), typeof(AppsettingsRetriever));
+            //service.AddScoped(typeof(IAppLogic), typeof(AppLogic));
 
             //configuration are heavily used in the .net core DI
             //for configuring services, so can be done usage of that
             var configurationBuilder = new ConfigurationBuilder();
-            configurationBuilder.SetBasePath()
+            //adding default configuration file
+            configurationBuilder.SetBasePath(GetAppsettingpath()).AddJsonFile("appsettings.json");
 
+            // building the configuration
+            var configuration = configurationBuilder.Build();
 
+            //Inject the configuration into the DI system
+            service.AddSingleton<IConfiguration>(configuration);
+            
+
+            // builder service provider
+            ServiceProvider = service.BuildServiceProvider();
+
+            
+        }
+
+        public string GetAppsettingpath()
+        {
+            var path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
+
+            Regex appPathMatcher = new Regex(@"(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+bin)");
+
+            var appRootPath = appPathMatcher.Match(path).Value;
+
+            return appRootPath;
+            //return Path.Combine(appRootPath, "appsettings.json");
         }
     }
 }
--- FileDuplicator/Program.cs
+++ FileDuplicator/Program.cs
@@ -3,7 +3,7 @@ using CommandLine.Text;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using FileDuplicator.Configuration;
+
 
 
 
@@ -13,12 +13,19 @@ namespace FileDuplicator
     {
         private static void Main(string[] args)
         {
+            var di = new DependencyInjection();
+            di.Builder();
+
+            var obj = di.ServiceProvider.GetService(typeof(AppLogic)) as AppLogic;
+            obj.Start(args[0],false);
             
+            
+
             Parser.Default.ParseArguments<Option>(args).
                 WithParsed(x =>
                 {
-                    var appLogic = new AppLogic(new AppsettingsRetriever());
-                    appLogic.Start(x.ChosenFolder.First(), x.IsBrowserLink);
+                    //var appLogic = new AppLogic(new AppsettingsRetriever());
+                    //appLogic.Start(x.ChosenFolder.First(), x.IsBrowserLink);
                 });
             
         }
